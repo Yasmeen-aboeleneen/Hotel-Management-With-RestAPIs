befe4cda5f4f93384b406868ffcfad3bc8066714
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-09-19 12:25:38 +0300

    Sign Up Screen Done

--- lib/Views/Auth/sign_up_screen.dart
+++ lib/Views/Auth/sign_up_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hotel_management/Core/Constants/colors.dart';
+import 'package:hotel_management/Views/Widgets/auth_widgets/sign_up_form.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    var h = MediaQuery.of(context).size.height;
+    var w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: kPrimary,
+      body: Padding(
+        padding: EdgeInsets.only(top: h * .07, left: w * .03, right: w * .03),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  'Sign Up to OCIEL',
+                  style: GoogleFonts.aBeeZee(
+                      color: kveryWhite,
+                      fontSize: w * .08,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: h * .06,
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                    top: h * .02, left: w * .02, right: w * .02),
+                height: h * .65,
+                width: w,
+                decoration: const BoxDecoration(
+                    color: kveryWhite,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(52),
+                        bottomRight: Radius.circular(52))),
+                child: const Center(child: SignUpForm()),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Views/Widgets/auth_widgets/sign_up_form.dart
+++ lib/Views/Widgets/auth_widgets/sign_up_form.dart
@@ -3,13 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hotel_management/Core/Constants/colors.dart';
 import 'package:hotel_management/Core/widgets/custom_normal_button.dart';
 import 'package:hotel_management/Core/widgets/custom_text_field.dart';
-import 'package:hotel_management/Views/Auth/sign_up_screen.dart';
+import 'package:hotel_management/Views/Auth/login_screen.dart';
 import 'package:hotel_management/Views/Home/home_screen.dart';
-import 'package:hotel_management/Views/Widgets/auth_widgets/login_row.dart';
 import 'package:hotel_management/Views/Widgets/auth_widgets/social_login.dart';
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({super.key});
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +17,15 @@ class LoginForm extends StatelessWidget {
     return ListView(
       scrollDirection: Axis.vertical,
       children: [
+        const CustomTextfield(
+          hintText: 'Enter your user name',
+          labelText: 'User Name',
+          iconData: Icons.person,
+          keyboardType: TextInputType.text,
+        ),
+        SizedBox(
+          height: h * .03,
+        ),
         const CustomTextfield(
           hintText: 'Enter your email',
           labelText: 'Email',
@@ -36,34 +44,24 @@ class LoginForm extends StatelessWidget {
         SizedBox(
           height: h * .02,
         ),
-        const LoginRow(),
-        SizedBox(
-          height: h * .03,
-        ),
         const Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             SocialLogin(image: 'assets/images/google.jpg', text: 'Google'),
             SocialLogin(image: 'assets/images/facebook.png', text: 'Facebook'),
-            SocialLogin(
-              icon: Icons.fingerprint,
-              text: 'Biometrics',
-            )
           ],
         ),
         SizedBox(
-          height: h * .04,
+          height: h * .02,
         ),
         GestureDetector(
             onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SignUpScreen()));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Loginscreen()));
             },
             child: Center(
               child: Text(
-                "Don't have an account ? Signup",
+                "Already have an account ? Login",
                 style: GoogleFonts.notoSansOlChiki(
                     fontWeight: FontWeight.bold,
                     fontSize: w * .04,
@@ -76,7 +74,7 @@ class LoginForm extends StatelessWidget {
           height: h * .015,
         ),
         CustomNormalButton(
-          title: 'Login',
+          title: 'Sign Up',
           onTap: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const HomeScreen()));
